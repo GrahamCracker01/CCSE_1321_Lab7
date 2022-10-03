2df0d77a0d48837e7304b4a2b5fc9bdd5efb71ad
GrahamCracker01 <mrgrahamcracker01@gmail.com>
2022-10-02 22:00:06 -0400

    Initial commit

--- src/Lab7A.java
+++ src/Lab7A.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Lab7A {
+//Class: CSE 1321L
+//Section: J02
+///Term: Fall 2022
+//Instructor: Aarthi Poovalingam
+//Name: Asher Graham
+//Lab#: ...
+
+import java.util.Scanner;
+class Lab7A {
+    public static void main(String[] args){
+
+        //declare scanner and variables
+        Scanner scan = new Scanner(System.in);
+        int intSize;
+
+        //get input
+        System.out.print("Please enter a value for the size: ");
+        intSize = scan.nextInt();
+
+        //output
+        System.out.println("This is the requested " + intSize + "x" + intSize + " box:");
+        for (int rows = 0; rows < intSize; rows++) {
+            for (int columns = 0; columns < intSize; columns++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
 }
